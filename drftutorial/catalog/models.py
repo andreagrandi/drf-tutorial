235dfcc338bdfbf775a9bc2d153d31cace3b7188
Andrea Grandi <a.grandi@gmail.com>
2016-09-21 17:06:25 +0100

    Initial implementation of the API to return products. Function based.

---
+++ drftutorial/catalog/models.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=20)
---
+++ drftutorial/catalog/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+
+def product_list(request):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return JSONResponse(serializer.data)
