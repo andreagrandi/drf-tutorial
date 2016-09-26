84ee0b55021592235f590aa4cc52cc8b13800c32
Andrea Grandi <a.grandi@gmail.com>
2016-09-26 15:20:29 +0100

    Implement GET Product using ListAPIView

--- drftutorial/catalog/views.py
+++ drftutorial/catalog/views.py
@@ -1,12 +1,10 @@
 from django.http import HttpResponse
-from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 
 
-class ProductList(APIView):
-    def get(self, request, format=None):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+class ProductList(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
