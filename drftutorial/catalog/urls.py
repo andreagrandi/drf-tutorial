110605532f178d42b0831792dc9ad936694a07cc
Andrea Grandi <a.grandi@gmail.com>
2016-09-26 15:16:59 +0100

    Implementing GET Product list with APIView

--- drftutorial/catalog/urls.py
+++ drftutorial/catalog/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^products/$', views.product_list),
+    url(r'^products/$', views.ProductList.as_view()),
 ]
--- drftutorial/catalog/views.py
+++ drftutorial/catalog/views.py
@@ -1,22 +1,12 @@
 from django.http import HttpResponse
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 
 
-class JSONResponse(HttpResponse):
-    """
-    An HttpResponse that renders its content into JSON.
-    """
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-
-
-def product_list(request):
-    if request.method == 'GET':
+class ProductList(APIView):
+    def get(self, request, format=None):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
