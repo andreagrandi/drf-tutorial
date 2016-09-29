68eea447796723573dfa919f1dcf5d1b83d23bf3
Andrea Grandi <a.grandi@gmail.com>
2016-09-29 16:59:20 +0100

    Implementing the POST method for ListProducts with ListCreateAPIView

--- drftutorial/catalog/views.py
+++ drftutorial/catalog/views.py
@@ -1,20 +1,10 @@
 from django.http import HttpResponse
+from rest_framework import generics
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
 
 
-class ProductList(APIView):
-    def get(self, request, format=None):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
