2be2b71dbd3aba4d7aee2c54102eeac45252c5ed
Andrea Grandi <a.grandi@gmail.com>
2016-09-29 16:12:34 +0100

    Add POST method to ProductList class

--- drftutorial/catalog/views.py
+++ drftutorial/catalog/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -10,3 +11,10 @@ class ProductList(APIView):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
