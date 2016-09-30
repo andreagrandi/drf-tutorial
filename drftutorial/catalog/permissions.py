2407d126a8918b99f72cdadf5ef7ecfc9021d5a4
Andrea Grandi <a.grandi@gmail.com>
2016-09-30 10:47:41 +0100

    Add custom permission IsAdminOrReadonly class to ProductList view

---
+++ drftutorial/catalog/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        else:
+            return request.user.is_staff
--- drftutorial/catalog/views.py
+++ drftutorial/catalog/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from rest_framework import generics
 from rest_framework.response import Response
+from .permissions import IsAdminOrReadOnly
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -8,3 +9,4 @@ from .serializers import ProductSerializer
 class ProductList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = (IsAdminOrReadOnly, )
