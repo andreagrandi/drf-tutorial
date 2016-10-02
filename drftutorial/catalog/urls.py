f1f8dbf4a76e2d4e2ba3b9d34f0ee9431883fbb8
Andrea Grandi <a.grandi@gmail.com>
2016-10-02 20:33:32 +0100

    Add ReviewDetail API to handle details of a product review

--- drftutorial/catalog/urls.py
+++ drftutorial/catalog/urls.py
@@ -3,6 +3,13 @@ from . import views
 
 urlpatterns = [
     url(r'^products/$', views.ProductList.as_view()),
-    url(r'^products/(?P<pk>[0-9]+)/$', views.ProductDetail.as_view()),
-    url(r'^products/(?P<pk>[0-9]+)/reviews/$', views.ReviewList.as_view()),
+    url(r'^products/(?P<product_id>[0-9]+)/$', views.ProductDetail.as_view()),
+    url(
+        r'^products/(?P<product_id>[0-9]+)/reviews/$',
+        views.ReviewList.as_view()
+    ),
+    url(
+        r'^products/(?P<product_id>[0-9]+)/reviews/(?P<review_id>[0-9]+)/$',
+        views.ReviewDetail.as_view()
+    ),
 ]
--- drftutorial/catalog/views.py
+++ drftutorial/catalog/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from .permissions import IsAdminOrReadOnly
+from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Product, Review
 from .serializers import ProductSerializer, ReviewSerializer
@@ -9,20 +9,36 @@ class ProductList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = (IsAdminOrReadOnly, )
+    lookup_url_kwarg = 'product_id'
 
 
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = (IsAdminOrReadOnly, )
+    lookup_url_kwarg = 'product_id'
 
 
 class ReviewList(generics.ListCreateAPIView):
-    queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, )
+    lookup_url_kwarg = 'product_id'
 
     def perform_create(self, serializer):
         serializer.save(
             created_by=self.request.user,
-            product_id=self.kwargs['pk'])
+            product_id=self.kwargs['product_id'])
+
+    def get_queryset(self):
+        product = self.kwargs['product_id']
+        return Review.objects.filter(product__id=product)
+
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ReviewSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+    lookup_url_kwarg = 'review_id'
+
+    def get_queryset(self):
+        review = self.kwargs['review_id']
+        return Review.objects.filter(id=review)
